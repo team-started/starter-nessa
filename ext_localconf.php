9dfd5b1e21df95e6870ebe47b9e6b23505115f1f
Christian <c.wolfram@chriwo.de>
2022-02-18 22:30:49 +0000

    [FEATURE][#34] Aktualisierung auf TYPO3 v11.5

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,13 @@
 <?php
 
-defined('TYPO3_MODE') || die();
+defined('TYPO3') || die();
 
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['nessa'] = 'EXT:starter_nessa/Configuration/RTE/Nessa.yaml';
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['nessa-minimal'] = 'EXT:starter_nessa/Configuration/RTE/NessaMinimal.yaml';
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['nessa-headlines'] = 'EXT:starter_nessa/Configuration/RTE/NessaHeadlines.yaml';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['starterteam'][] = 'StarterTeam\StarterNessa\ViewHelpers';
 
-if (TYPO3_MODE === 'BE') {
+if (\TYPO3\CMS\Core\Http\ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend()) {
     // Add default UserTSConfig
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
         "@import 'EXT:starter_nessa/Configuration/TSConfig/User/Default.typoscript'"
