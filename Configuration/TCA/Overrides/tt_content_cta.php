09aa06b1e94b215cd85587b4b1c350bad033998f
Christian <c.wolfram@chriwo.de>
2022-11-12 09:44:20 +0000

    [BUGFIX] Fix missing CTA fields in CE-NessaCta

--- Configuration/TCA/Overrides/tt_content_cta.php
+++ Configuration/TCA/Overrides/tt_content_cta.php
@@ -11,6 +11,7 @@ defined('TYPO3') || die();
             --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
+                --palette--;;starterCta,
                 bodytext,
             --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
